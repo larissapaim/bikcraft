46b6f9527b0094d0aee1c7148e4bcc8a602d66f5
Projeto Origamid <larissa.paimb19@gmail.com>
2023-05-16 10:24:56 -0300

    JavaScript em perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const links = document.querySelectorAll('.header-menu a');
+// Ativar Links do Menu
+
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href;
@@ -9,3 +11,36 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa)
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
